06bdbf7c14b4dec0435257420f67125e896d0839
成国栋 <chengguodong@growing.io>
2017-12-18 21:35:18 +0800

    开发中

--- src/test/scala/com/github/cgdon/squeue/benchmark/SpeedBenchmark.scala
+++ src/test/scala/com/github/cgdon/squeue/benchmark/SpeedBenchmark.scala
@@ -2,6 +2,7 @@ package com.github.cgdon.squeue.benchmark
 
 import java.io.File
 
+import com.github.cgdon.jtexttable.TextTable
 import com.github.cgdon.squeue.FQueue
 import org.apache.commons.io.FileUtils
 
@@ -10,25 +11,28 @@ object SpeedBenchmark extends App {
 
   val path = args(0)
   val dataFileMb = args(1).toInt
+
   val rootDir = new File(path)
 
-  val sizeArr = List.tabulate(5)(x => math.pow(10, x + 5).toInt)
+  val sizeArr = Array.tabulate(4)(x => math.pow(10, x + 4).toInt)
 
   val seed = ("a" * 1024).getBytes()
 
-  val header = Seq("size", "cost(ms)")
+  val header = Array("size", "cost(s)", "speed(msg/s)")
 
-  sizeArr.map { size =>
+  val res = sizeArr.map { size =>
+    FileUtils.deleteQuietly(rootDir)
     rootDir.mkdirs()
     val queue = new FQueue(rootDir, dataFileMb)
     val startTs = System.currentTimeMillis()
     for (_ <- 0 until size) {
       queue.add(seed)
     }
-    val endTs = System.currentTimeMillis()
-    FileUtils.deleteQuietly(rootDir)
-    Seq(size,(endTs-startTs))
+    val costSecond = (System.currentTimeMillis() - startTs) / 1000.0
+    queue.close()
+    Array(size.toString, costSecond.formatted("%.2f"), (size / costSecond).formatted("%.2f"))
   }
 
+  println(new TextTable(header, res))
 
 }
